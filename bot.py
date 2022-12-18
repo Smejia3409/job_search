167c542952d6cf3bddf9134031c99b725df5e6e1
Sandro Mejia <sandromejia3409@gmail.com>
2022-12-18 00:27:46 -0500

    Made job results print out in table

--- bot.py
+++ bot.py
@@ -4,15 +4,29 @@ from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from prettytable import PrettyTable
+
+
+def job_table(jobs):
+    table = PrettyTable()
+    table.field_names = ["Job Title", "Link"]
+
+    for job in jobs:
+        title = job.find_element(by=By.TAG_NAME, value="span").get_attribute("innerHTML")
+        link = job.find_element(by=By.TAG_NAME, value="a").get_attribute("href")
+        table.add_row([title, link])
+
+    print(table)
+
 
 class Bot(webdriver.Chrome):
     def __init__(self):
         options = Options()
         options.add_experimental_option("detach", True)
-        #keeps browser on
+        # keeps browser on
         self.teardown = False
         # makes window full screen
-        super(Bot, self).__init__( service=Service(ChromeDriverManager().install()), options=options)
+        super(Bot, self).__init__(service=Service(ChromeDriverManager().install()), options=options)
         self.implicitly_wait(5)
         self.maximize_window()
 
@@ -20,16 +34,16 @@ class Bot(webdriver.Chrome):
         if self.teardown:
             self.quit()
 
-
-
     def open_page(self):
-        print("openning indeed")
+        print("Opening indeed")
         self.get("https://indeed.com")
 
+    def search(self, ):
+        print("Enter job type")
+        job = input()
+        print("Enter location")
+        location = input()
 
-
-
-    def search(self, job, location):
         # finding input id for job and location input
         job_input = self.find_element(by=By.ID, value="text-input-what")
         location_input = self.find_element(by=By.ID, value="text-input-where")
@@ -38,14 +52,17 @@ class Bot(webdriver.Chrome):
         job_input.send_keys(job)
         location_input.send_keys(location)
         job_input.send_keys(Keys.ENTER)
-
+        self.get_jobs()
 
     def get_jobs(self):
         job_list = self.find_elements(by=By.CLASS_NAME, value="resultContent")
 
-        for job in job_list:
-            title = job.find_element(by=By.TAG_NAME, value="span")
-            link = job.find_element(by=By.TAG_NAME, value="a")
-            print(title.get_attribute("innerHTML"), link.get_attribute("href"))
-        if(len(job)==0):
-            print("No results found")
+        if not job_list:
+            print("No results found, Do you want to do another search?")
+            res = input()
+            if res.lower() == "yes":
+                self.search()
+            else:
+                print("Thank you for using, have a nice day")
+        else:
+            job_table(job_list)
--- run.py
+++ run.py
@@ -2,11 +2,6 @@ from bot import Bot
 
 with Bot() as bot:
     bot.open_page()
-    print("Enter job type")
-    job = input()
-    print("Enter location")
-    location = input()
-    bot.search(job, location)
-    bot.get_jobs()
+    bot.search()
 
 
